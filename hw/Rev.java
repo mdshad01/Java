529ce3838f19a0b55617e9da199d1a91da0f2f2a
Shad <shadalam0024@gmail.com>
2025-06-11 10:14:28 +0530

    First program is created

--- hw/Rev.java
+++ hw/Rev.java
@@ -10,13 +10,41 @@ public class Rev {
         return n * fact(n - 1);
     }
 
+    public static int binarySerach(int arr[], int key) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] < key) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         // Rev r = new Rev();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
-        System.out.println(fact(n));
+        // System.out.print("Enter a number : ");
+        // int n = sc.nextInt();
+        // System.out.println(fact(n));
         // sc.close();
+
+        int arr[] = { 1, 3, 7, 9, 12 };
+        int key = 3;
+        System.out.println(binarySerach(arr, key));
 
     }
 }
